9381e5d36eba12281e40ad1e3bb0287ab550d18f
arthurlfig <arthurklfig@gmail.com>
2024-04-11 18:10:52 -0300

    atualização da lista

--- ex10.py
+++ ex10.py
@@ -1,9 +1,9 @@
 lista = [2, 4, 7, 2, 3, 3, 1, 0, 2, 6]
 frequente = 0
 maior = 0 
-for i in lista:
-    contagem = lista.count(i)
-    if contagem > maior:
+for i in range(1,11):
+     inst = lista.count(i)
+     if inst > maior:
         contagem = maior 
         frequente = i 
 print(frequente)
